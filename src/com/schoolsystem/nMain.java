e9a61148dad623d58f3bcde9704df0dbcc1ceae2
joemartbatulanon202-bit <joemartbatulanon202@gmail.com>
2026-08-12 15:27:59 +0800

    nMain.java

---
+++ src/com/schoolsystem/nMain.java
@@ -0,0 +1,26 @@
+package com.schoolsystem;
+
+import java.awt.EventQueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+
+                try {
+
+                    Login login = new Login();
+                    login.setVisible(true);
+
+                } catch (Exception e) {
+
+                    e.printStackTrace();
+
+                }
+            }
+        });
+    }
+}
