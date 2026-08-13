3a2e57e275672e69860ff4e708e38dbdef47cb18
joemartbatulanon202-bit <joemartbatulanon202@gmail.com>
2026-08-13 13:35:22 +0800

    insert_into_teachersAmy.java

---
+++ src/com/schoolsystem/insert_into_teachersAmy.java
@@ -0,0 +1,6 @@
+USE school_management_system;
+
+INSERT INTO teachers
+(teacher_number, first_name, last_name, subject, contact_number)
+VALUES
+('T-0001', 'Maria', 'Santos', 'Computer Programming', '09123456789');
