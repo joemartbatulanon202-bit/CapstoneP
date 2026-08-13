a1820c668913770c44044cbaf3bcf4c1f5261f68
joemartbatulanon202-bit <joemartbatulanon202@gmail.com>
2026-08-13 13:28:32 +0800

    INSERT_INTO_studentsAMy.java

---
+++ src/com/schoolsystem/INSERT _INTO_studentsAMy.java
@@ -0,0 +1,6 @@
+USE school_management_system;
+
+INSERT INTO students
+(student_number, first_name, last_name, gender, strand, section, contact_number)
+VALUES
+('2026-0001', 'Juan', 'Dela Cruz', 'Male', 'ICT', 'ICT-12A', '09123456789');
