50784acc12dc6954c1a94e9cc0b732f40d575625
joemartbatulanon202-bit <joemartbatulanon202@gmail.com>
2026-08-13 13:43:57 +0800

    insert into subjectAMy.java

---
+++ src/com/schoolsystem/insert into subjectAMy.java
@@ -0,0 +1,6 @@
+USE school_management_system;
+
+INSERT INTO subjects
+(subject_code, subject_name, description)
+VALUES
+('CSS', 'Computer Systems Servicing', 'Computer hardware servicing, installation, maintenance, and troubleshooting');
